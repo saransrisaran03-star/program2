49c0eaebe54df16637deed78eb4319e1c791e543
SRI SARAN P <saransrisaran03@gmail.com>
2026-09-25 10:27:20 +0530

    Update answers.sql

--- starter/answers.sql
+++ starter/answers.sql
@@ -1,1 +1,14 @@
-
+use pickadb;
+CREATE TABLE student
+(
+studentID int(5)PRIMARY KEY,
+STUDENTnAME VARCHAR(20)NOT NULL,
+DOB DATE DEFAULT NULL,
+Gender VARCHAR(10)NOT NULL,
+DepartmentID int(5),
+CONSTRAINT UQ_studentName UNIQUE(studentName),
+CONSTRAINT FK_Department 
+FOREIGN KEY(DepartmentID)
+references Department(DEPARTMENTID)
+);
+desc student;
